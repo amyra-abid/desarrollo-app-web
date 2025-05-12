fa3c9fc210a408f9cae1c423223ef043d29df4a5
amyra-abaid <amyrasandoval@liceovvh.cl>
2025-05-12 10:10:57 -0400

    iii

---
+++ JS/01_introduccion_js_tipos_datos/script.js
@@ -0,0 +1,14 @@
+//prueba de conexion JS
+console.log("conexiones establecida con js!");
+
+//creaccion de variables
+
+let num=25; //variable INT (numeron entero)
+function numeroentero(){
+    return num;
+}
+
+//impresion en consola
+console.log(numeroEntero()); //Mostrar Numero Entero
+
+//creacion de numero decimal
